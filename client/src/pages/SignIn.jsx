4071b1e9156cd40faa90226b02a0ea00012d688f
Muhannad-Satouf <muhannad.satouf@greatit.se>
2023-10-10 16:38:53 +0200

    Fix sign in page Bugg

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-    signInStart,
-    signInSuccess,
-    signInFailure,
-} from '../redux/user/userSlice';
 
 export default function SignIn() {
     const [formData, setFormData] = useState({});
-    const { loading, error } = useSelector((state) => state.user);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
-    const dispatch = useDispatch();
     const handleChange = (e) => {
         setFormData({
             ...formData,
@@ -21,7 +15,7 @@ export default function SignIn() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            dispatch(signInStart());
+            setLoading(true);
             const res = await fetch('/api/auth/signin', {
                 method: 'POST',
                 headers: {
@@ -30,17 +24,20 @@ export default function SignIn() {
                 body: JSON.stringify(formData),
             });
             const data = await res.json();
-            console.log(data);
             if (data.success === false) {
-                dispatch(signInFailure(data.message));
+                setLoading(false);
+                setError(data.message);
                 return;
             }
-            dispatch(signInSuccess(data));
+            setLoading(false);
+            setError(null);
             navigate('/');
         } catch (error) {
-            dispatch(signInFailure(error.message));
+            setLoading(false);
+            setError(error.message);
         }
     };
+
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
@@ -68,9 +65,9 @@ export default function SignIn() {
                 </button>
             </form>
             <div className='flex gap-2 mt-5'>
-                <p>Dont have an account?</p>
+                <p>Don not have an account?</p>
                 <Link to={'/sign-up'}>
-                    <span className='text-blue-700'>Sign up</span>
+                    <span className='text-blue-700'>Sign Up</span>
                 </Link>
             </div>
             {error && <p className='text-red-500 mt-5'>{error}</p>}
