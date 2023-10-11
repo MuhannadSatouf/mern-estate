01277d4305322975e3d01d5fd7bea96d2677aef1
Muhannad-Satouf <muhannad.satouf@greatit.se>
2023-10-11 15:22:47 +0200

    Private Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React from 'react';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import Header from './Components/Header';
+import PrivateRoute from './Components/PrivateRoute';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
@@ -16,7 +16,9 @@ export default function App() {
         <Route exact path="/about" element={<About />} />
         <Route exact path="/sign-in" element={<SignIn />} />
         <Route exact path="/sign-up" element={<SignUp />} />
-        <Route exact path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route exact path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
