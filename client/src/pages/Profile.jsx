280c9b0ffc3fbf3bcfa6a9ae165641350e3ba4a7
Muhannad-Satouf <muhannad.satouf@greatit.se>
2023-10-12 12:15:30 +0200

    Image profile upload

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,68 @@
 import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
+    const fileReference = useRef(null);
     const { currentUser } = useSelector((state) => state.user);
+    const [file, setFile] = useState(undefined);
+    const [fileUploadProgress, setFileUploadProgress] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+
+    useEffect(() => {
+        if (file) {
+            handleFileSubmit(file);
+        }
+    }, [file]);
+
+    const handleFileSubmit = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setFileUploadProgress(Math.round(progress));
+            },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                //I am getting an error here telling me hat firebase post request is denied
+
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+                    setFormData({ ...formData, avatar: downloadURL })
+                );
+            }
+        );
+    };
+
+
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-            <form className='flex flex-col'>
-                <img src={currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+            <form className='flex flex-col gap-4'>
+                <input type='file' ref={fileReference} hidden accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
+                <img onClick={() => fileReference.current.click()} src={formData.avatar || currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+                <p className='text-sm self-center'>
+                    {fileUploadError ? (
+                        <span className='text-red-700'>
+                            Error Image upload (image must be less than 2 mb)
+                        </span>
+                    ) : fileUploadProgress > 0 && fileUploadProgress < 100 ? (
+                        <span className='text-slate-700'>{`Uploading ${fileUploadProgress}%`}</span>
+                    ) : fileUploadProgress === 100 ? (
+                        <span className='text-green-700'>Image successfully uploaded!</span>
+                    ) : (
+                        ''
+                    )}
+                </p>
                 <input type='text' placeholder='username' id="username" className='border p-3 rounded-lg' />
                 <input type='email' placeholder='email' id="email" className='border p-3 rounded-lg' />
                 <input type='password' placeholder='password' id="password" className='border p-3 rounded-lg' />
