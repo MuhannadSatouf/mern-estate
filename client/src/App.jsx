c8c5c6057d09d8bc9add72751cbb4f69cd900762
Muhannad-Satouf <muhannad.satouf@greatit.se>
2023-10-16 11:32:55 +0200

    Another deployment test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,8 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
-import Header from './pages/comp/Header';
-import PrivateRoute from './pages/comp/PrivateRoute';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
